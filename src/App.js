0d78d127c3f91d22ebe506c203f022708ffe5fb8
adriano sabelli <adrianosabelli@192.168.1.31>
2023-10-12 11:55:32 +0200

    secondo commit della fantastica app fontane

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import FontanaMap from './FontanaMap'
 
+const fontane = [
+  { id: 1, lat: 51.505, lng: -0.09, nome: "Fontana 1" },
+  { id: 2, lat: 41.382128, lng: 2.187600,  nome: "Carre de Balboa 10 BCN" },
+  // ... altre fontane
+];
+
 function App() {
   const position = [51.505, -0.09];
   return (
     <div>
-      <h1>Benvegnuti nell App fontane!</h1>
-      <FontanaMap position = {position}/>
+      <h1>Benvegnuti nell App fontanes!</h1>
+      <FontanaMap position = {position} fontane = {fontane} />
     </div>
   );
 }
--- src/FontanaMap.js
+++ src/FontanaMap.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
-function FontanaMap({ position }) {
+const fontanaIcon = new L.Icon({
+  iconUrl: process.env.PUBLIC_URL + '/fountain_icon_138551.png',
+  iconSize: [32, 32], // dimensioni dell'icona
+  iconAnchor: [16, 32], // punto dell'icona che corrisponderà esattamente alla posizione del marker
+  popupAnchor: [0, -32] // punto da cui apparirà il popup
+});
+
+
+function FontanaMap({ fontane, position }) {
   return (
-    <MapContainer center={position} zoom={13} style={{ width: '100%', height: '400px' }}>
+    <MapContainer center={position} zoom={4} style={{ width: '100%', height: '400px' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position}>
-        <Popup>Una fontana qui!</Popup>
-      </Marker>
+      />{fontane.map((fontana)=>(<Marker
+        key={fontana.id}
+        position={[fontana.lat, fontana.lng]}
+        icon = {fontanaIcon}
+        // ... altre props, come l'icona
+      >
+        <Popup>{fontana.nome}</Popup>
+      </Marker>))}
+        
+        
+      
     </MapContainer>
   );
 }
